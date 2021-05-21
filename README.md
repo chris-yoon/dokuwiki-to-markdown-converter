4487b186e39d2dcf14f4694bf808eda1b8754416
Chris Yoon <chris.cw.yoon@gmail.com>
2021-05-21 15:15:32 +0900

    regex revised

--- README.md
+++ README.md
@@ -1,30 +1,39 @@
 # How to convert dokuwiki to markdown
+
 It will be helpful when there are too many dokuwiki files to convert.
 To convert dokuwiki to markdown I will use a document conversion tool called pandoc.
 And also I will use the following basic sentence structure to automate time-consuming tasks.
-> for {each item} in {a collection of items} do {command} 
+
+> for {each item} in {a collection of items} do {command}
 
 ## Installation (pandoc)
+
 Pandoc is a free and open-source document converter. It was created by John MacFarlane, a philosophy professor at the University of California, Berkeley. (10 August 2006)
 
-* https://pandoc.org/installing.html
-* Check the pandoc version in the cmd
+- https://pandoc.org/installing.html
+- Check the pandoc version in the cmd
+
 ```
 pandoc --version
 ```
 
 ## Usage
+
 ### 1. Convert dokuwiki files to md files using pandoc
+
 ```
 cd d:/markdown
 for /r %i in (*.txt) do pandoc -f dokuwiki -t markdown %~fi -o %~dpni.md
 ```
+
 > for /r : Loop through files (Recurse subfolders)
-> 
+
 ### 2. Change the names of the url-encoded md files to Korean.
+
 ```
 for /r %i in (*.md) do @node uri_decode.js %~pi %~ni %~xi %~di >> rename_decoded_ko.bat
 ```
+
 ```
 -- process.argv mapping
 -- process.argv[0]: node
@@ -38,43 +47,67 @@ console.log('rename ' + process.argv[5] + process.argv[2] + process.argv[3] + pr
 -- result
 rename D:\markdown\pages\egovframework\%EA%B2%8C%EC%8B%9C%EB%AC%BC%ED%86%B5%EA%B3%84.md 게시물통계.md
 ```
+
 run the following batch file from the command line
+
 ```
 rename_decoded_ko.bat
 ```
 
 ### 3. Delete all dokuwiki txt files
+
 ```
 for /r %i in (*.txt) do del %~fi
 ```
 
 ### 4. Change the url-encoded image filenames to Korean.
+
 ```
 for /r %i in (*.gif, *.jpg, *.jpeg, *.png) do @node uri_decode.js %~pi %~ni %~xi %~di >> rename_decoded_ko_img.bat
 ```
+
 run the following batch file from the command line
+
 ```
 rename_decoded_ko_img.bat
 ```
+
 ### 5. Delete unnecessary files
+
 ```
 for /r %i in (*.pdf, *.zip, *.doc, *.ppt, *.tar) do del %~fi
 ```
 
 ### 6. regexp replace 적용
+
 It is necessary to change the image path within the md file. For example, the following image paths applied to dokuwiki can be different in markdown:
+
 ```
 -- as-is
 ![공통분류코드목록 화면](/egovframework/com/sym/ccc/cmmnclcodelist.jpg)
 -- to-be
 ![공통분류코드목록 화면](../../media/egovframework/com/sym/ccc/cmmnclcodelist.jpg)
 ```
+
+```
+-- starting with '/' and ending with '.jpg' or '.png' or '.gif'
+-- flag: global, case insensitive, multilne
+/\/.*\.(jpg|png|gif)/gim
+```
+
 The following absolute path link isn't at all recognized as a link.
+
 ```
 -- as-is
 -   실행환경 참조 : [scheduling](/egovframework/rte/fdl/scheduling)
 -- to-be
 -   실행환경 참조 : [scheduling](../egovframework/rte/fdl/scheduling)
+```
+
+```
+-- starting with '/' and ending in not '.jpg' or '.png' or '.gif'
+-- flag: global, case insensitive, multilne
+/\(.*[^\.(jpg|png|gif)]\)/gim
 ```
 
 ## Reference
@@ -91,3 +124,7 @@ for /r %i in (*.md) do @echo %~xi
 -
 for /r %i in (*.md) do @echo %~fi
 ```
+
+### RegExr
+
+- [regexr.com/5mhou](https://regexr.com/5ml92)
--- regex_test.js
+++ regex_test.js
@@ -1,21 +1,21 @@
 function idCheck(data) {
-    var titleCheck = data;
-    var languageCheck = /[ㄱ-ㅎ|ㅏ-ㅣ|가-힣]/;
-    if (languageCheck.test(titleCheck)) {
-        console.log("ID에 한글이 포함되어 있습니다.");
-        return;
-    }
+  var titleCheck = data;
+  var languageCheck = /[ㄱ-ㅎ|ㅏ-ㅣ|가-힣]/;
+  if (languageCheck.test(titleCheck)) {
+    console.log("ID에 한글이 포함되어 있습니다.");
+    return;
+  }
 }
 
 function checkNumber(data) {
-    let checkData = data;
-    if(checkData.length > 18) {
-        data = checkData.substring(0,8);
-    } else {
-        //숫자가 아닌 문자 모두 제거
-        data = checkData.replace(/[^0-9]/g, '');
-    }
-    console.log(data)
+  let checkData = data;
+  if (checkData.length > 18) {
+    data = checkData.substring(0, 8);
+  } else {
+    //숫자가 아닌 문자 모두 제거
+    data = checkData.replace(/[^0-9]/g, "");
+  }
+  console.log(data);
 }
 
 //checkNumber("ab12a34c3d");
@@ -28,46 +28,37 @@ function checkNumber(data) {
 //![](/images/adt/개발환경목록.gif)
 //![표준관리전체프로세스](/egovframework/adt/표준관리_전체프로세스.jpg)
 
-
 function parseMd(text) {
-    const codeblock = /```([^]+?.*?[^]+?[^]+?)```/g
-    const code = /`(.*?)`/g
-    const link = /\[(.*?)\]\((.*?)\)/g
-    const paragraph = /(.+((\r?\n.+)*))/g
-    const link_with_ext = /\[(.*?)\]\((.*?\.(.*?))\)/g
-    const link_dir_with_ext = /\[(.*?)\]\(\/(.*?)\/(.*?\.(.*?))\)/g
-    const end_with_jpg = /jpg$|png$/g //jpg, png 로 끝나는 문자열 체크
-    const image_extiontions = /jpg|png|gif/g //값이 jpg 또는 png 또는 gif
+  const link_with_ext = /\[(.*?)\]\((.*?\.(.*?))\)/gim;
+  const url_end_with_image_ext = /\[(.*?)\]\((.*?\.(jpg|png|gif))\)/gim;
+  const url_end_with_not_image_ext = /\[(.*?)\]\((.*?[^\.(jpg|png|gif)])\)/gim;
 
-    var txtResult = text.replace(link_with_ext, '$2');
-    var extention = text.replace(link_with_ext, '$3');
-    console.log(extention);
+  //1st Group : String in bracket []
+  //2nd Group : String in parenthesis ()
+  var txtMutable = text.replace(link_with_ext, "$2");
+  //3rd Group : Extenstion in parenthesis ()
 
-    if(image_extiontions.test(extention)) {
-        console.log("images")
-    }
-
-    if (end_with_jpg.test(txtResult)) {
-        console.log("다음은 이미지 파일입니다.");
-        return text.replace(link_with_ext, '[$1]($2)')
-        
-    } else  {
-        console.log("다음은 이미지 파일이 아닙니다.");
-        return text.replace(link_with_ext, '[$1]($2.md)')
-    }
-
-//    return text.replace(codeblock, '<pre><code>$1</code></pre>')
-//    .replace(code, '<code>$1</code>')
-    //.replace(link, '<a href="$2">$1</a>')
-    //.replace(paragraph, '<p>$1</p>')
-    //.replace(link_dir_with_ext, '1: $1, 2: $2, 3: $3, 4: $4');;
-//    .replace(link_with_ext, '[$1]($2.md)')
-    //.replace(".jpg", '');
-
+  if (url_end_with_image_ext.test(text)) {
+    return text.replace(link_with_ext, "[$1](../../media$2)");
+  } else if (url_end_with_not_image_ext.test(text)) {
+    return text.replace(link_with_ext, "[$1](..$2)");
+  }
 }
 
-//console.log(parseMd("```test```"))
-console.log(parseMd("[표준관리전체프로세스](/egovframework/adt/표준관리_전체프로세스.jpg)"))
-console.log(parseMd("[표준프레임워크 모바일 디바이스 API 개발환경](/egovframework/hyb3.9/hdev)"))
-console.log(parseMd("[표준관리전체프로세스](/egovframework/adt/표준관리_전체프로세스.jpg)"))
+console.log(
+  parseMd(
+    "[표준관리전체프로세스](/egovframework/adt/표준관리_전체프로세스.jpg)"
+  )
+);
 
+console.log(
+  parseMd(
+    "[표준프레임워크 모바일 디바이스 API 개발환경](/egovframework/hyb3.9/hdev)"
+  )
+);
+
+console.log(
+  parseMd(
+    "[표준관리전체프로세스](/egovframework/adt/표준관리_전체프로세스.gif)"
+  )
+);
